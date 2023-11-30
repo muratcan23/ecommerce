d7e5d5aae3eb14c46d23654804925d974412201c
Murat Can <muratal0606@gmail.com>
2023-12-01 01:43:10 +0300

    add image URL manually

--- app/components/home/ProductCard.tsx
+++ app/components/home/ProductCard.tsx
@@ -1,10 +1,19 @@
+import textClip from "@/public/utils/TextClip";
+import { Rating } from "@mui/material";
 import Image from "next/image";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
-    <div className="w-[240px] shadow-lg p-2">
-      <div className="relative">
-        <Image src={product.image} fill alt="" className="" />
+    <div className="w-[240px] cursor-pointer flex flex-col flex-1 shadow-lg p-2 rounded-md ">
+      <div className="relative h-[150px] ">
+        <Image src={product.image} fill alt="" className="object-contain" />
+      </div>
+      <div className="text-center mt-2">
+        <div>{textClip(product.name)}</div>
+        <Rating name="read-only" value={4} readOnly />
+        <div className="text-orange-600 font-bold  text-lg md:text-xl">
+          {product.price}{" "}
+        </div>
       </div>
     </div>
   );
--- app/components/home/Products.tsx
+++ app/components/home/Products.tsx
@@ -6,10 +6,10 @@ const Products = () => {
   return (
     <div>
       <Heading text="all products" center={false} />
-      <div>
-        {products.map((product) => {
-          <ProductCard key={product.id} product={product} />;
-        })}
+      <div className="flex items-center flex-wrap gap-3 md:gap-10">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
     </div>
   );
