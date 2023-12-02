2b58e727ba3cbe42981d3e0fd33be54cb0d606f0
Murat Can <muratal0606@gmail.com>
2023-12-02 22:57:24 +0300

    add product details

--- app/components/detail/DetailClient.tsx
+++ app/components/detail/DetailClient.tsx
@@ -1,11 +1,30 @@
 "use client";
 
+import Image from "next/image";
 import PageContainer from "../conatiners/PageContainer";
 
 const DetailClient = ({ product }: { product: any }) => {
   return (
     <div className="my-10">
-      <PageContainer>{product?.name}</PageContainer>
+      <PageContainer>
+        <div className="block  md:flex gap-10 justify-center">
+          <div className="relative h-[400px] w-[400px]  ">
+            <Image src={product.image} fill alt="" />
+          </div>
+          <div className="w-1/2">
+            <div className="text-xl md:text-2xl">{product?.name}</div>
+            <div className="text-slate-500">{product?.description} </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <div> STOCK STATUS</div>
+            {product.inStock ? (
+              <div className="text-green-500">In stock</div>
+            ) : (
+              <div className="text-red-500">out of stok</div>
+            )}
+          </div>
+        </div>
+      </PageContainer>
     </div>
   );
 };
