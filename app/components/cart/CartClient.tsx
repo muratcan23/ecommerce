408f44462a547c3961dffd454ad06be74dcc7b44
Murat Can <muratal0606@gmail.com>
2023-12-12 00:33:34 +0300

    add cart status

--- app/components/cart/CartClient.tsx
+++ app/components/cart/CartClient.tsx
@@ -6,9 +6,20 @@ import PageContainer from "../conatiners/PageContainer";
 const CartClient = () => {
   const { cartPrdcts } = useCart();
   console.log(cartPrdcts);
+  if (!cartPrdcts || cartPrdcts.length == 0) {
+    return <div>"There is no product in your cart..."</div>;
+  }
+
   return (
-    <div>
-      <PageContainer>Client cart</PageContainer>
+    <div className="my-3 md:my-10">
+      <PageContainer>
+        <div className="flex items-center gap-3 justify-between">
+          <div>Product Img</div>
+          <div>Product Name</div>
+          <div>Product quantity</div>
+          <div>Product Price</div>
+        </div>
+      </PageContainer>
     </div>
   );
 };
