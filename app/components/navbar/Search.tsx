d724453da95bf175d222060cf250e91ec91cd33e
Murat Can <muratal0606@gmail.com>
2023-12-23 08:02:49 +0300

    add search input at anvbar

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,5 +1,16 @@
 const Search = () => {
-  return <div className="hidden md:flex flex-1">Search</div>;
+  return (
+    <div className="hidden md:flex flex-1">
+      <input
+        className="py-2 px-3 border-none outline-none flex flex-1  rounded-l-md "
+        type="text"
+        placeholder="Search anything..."
+      />
+      <button className="p-2 bg-orange-800 cursor-pointer text-sm border-transparent">
+        Search
+      </button>
+    </div>
+  );
 };
 
 export default Search;
