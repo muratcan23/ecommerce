79f84536ec9e5d12794ae0f81f235ec4016b79c9
Murat Can <muratal0606@gmail.com>
2023-12-28 08:55:10 +0300

    give style to search bar

--- app/components/auth/RegisterClient.tsx
+++ app/components/auth/RegisterClient.tsx
@@ -1,9 +1,42 @@
 "use client";
 
+import { error } from "console";
 import Authcontainer from "../conatiners/AuthContainer";
+import Heading from "../general/Heading";
+import Input from "../general/Input";
 
 const RegisterClient = () => {
-  return <Authcontainer>register</Authcontainer>;
+  return (
+    <Authcontainer>
+      <div className="w-full md:w-[500px] p-3 shadow-lg rounded-md  ">
+        <Heading text="Register" center />
+        <Input
+          placeholder="Name"
+          type="text"
+          id="name"
+          register={register}
+          errors={error}
+          required
+        />
+        <Input
+          placeholder="Email"
+          type="text"
+          id="name"
+          register={register}
+          errors={error}
+          required
+        />
+        <Input
+          placeholder="Password"
+          type="text"
+          id="name"
+          register={register}
+          errors={error}
+          required
+        />
+      </div>
+    </Authcontainer>
+  );
 };
 
 export default RegisterClient;
