fcda77443e05641635cc5e289d339f1875edd585
muratcan23 <muratal0606@gmail.com>
2024-02-11 20:06:42 +0300

    implement firebase

---
+++ libs/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb_iVVZHNoM1WRXltmgqsWFrg4G8HdMDs",
+  authDomain: "shop-579da.firebaseapp.com",
+  projectId: "shop-579da",
+  storageBucket: "shop-579da.appspot.com",
+  messagingSenderId: "565513978355",
+  appId: "1:565513978355:web:5511eea2398a7857c09286",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
