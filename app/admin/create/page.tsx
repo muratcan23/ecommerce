d1b186be6ab6e69cc557e42ab586ed7f8b993239
muratcan23 <muratal0606@gmail.com>
2024-02-15 15:35:02 +0300

    correct file name

--- app/admin/create/page.tsx
+++ app/admin/create/page.tsx
@@ -1,5 +1,5 @@
 import { getCurrentUser } from "@/app/actions/getCurrentUser";
-import Createform from "@/app/components/admin/CreateForm";
+import CreateForm from "@/app/components/admin/CreateForm";
 import AuthContainer from "@/app/components/conatiners/AuthContainer";
 import WarningText from "@/app/components/navbar/WarningText";
 
@@ -11,7 +11,7 @@ const CreateProduct = async () => {
   }
   return (
     <AuthContainer>
-      <Createform />
+      <CreateForm />
     </AuthContainer>
   );
 };
