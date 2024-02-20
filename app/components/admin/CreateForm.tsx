9c31e1b4e9c1f0ed588c5a302f9bf424be011fe1
muratcan23 <muratal0606@gmail.com>
2024-02-21 01:40:54 +0300

    add useform values

--- app/components/admin/CreateForm.tsx
+++ app/components/admin/CreateForm.tsx
@@ -1,9 +1,28 @@
 "use client";
 
+import { FieldValues, useForm } from "react-hook-form";
 import Heading from "../general/Heading";
 import Input from "../general/Input";
 
 const CreateForm = () => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      description: "",
+      brand: "",
+      category: "",
+      price: "",
+      image: "",
+      inStock: false,
+    },
+  });
+
   return (
     <div>
       <Heading text="CreateForm" />
