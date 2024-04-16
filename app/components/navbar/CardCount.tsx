a27b3835825c625f9371d95d47f872ee373c90ad
muratcan23 <muratal0606@gmail.com>
2024-04-17 01:54:06 +0300

    add hover effect upon basket

--- app/components/navbar/CardCount.tsx
+++ app/components/navbar/CardCount.tsx
@@ -8,10 +8,13 @@ const CardCount = () => {
   return (
     <div
       onClick={() => router.push("/cart")}
-      className="hidden md:flex relative cursor-pointer"
+      className="hidden md:flex relative cursor-pointer group"
     >
-      <MdShoppingBasket size="30" />
-      <div className="absolute -top-1 -right-2 text-xs bg-orange-900 w-5 h-5 flex items-center justify-center rounded-full">
+      <MdShoppingBasket
+        size="30"
+        className="text-white group-hover:text-orange-900"
+      />
+      <div className="absolute -top-1 -right-2 text-xs bg-orange-900 w-5 h-5 flex items-center justify-center rounded-full group-hover:bg-gray-200 group-hover:text-orange-900">
         {cartPrdcts?.length}
       </div>
     </div>
