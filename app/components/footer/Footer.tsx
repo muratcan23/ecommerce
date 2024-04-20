a595bb2c368037b2ec425187b7ce512de7d83520
muratcan23 <muratal0606@gmail.com>
2024-04-21 02:13:33 +0300

    implement footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,7 +1,34 @@
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <div className="w-full h-24 bg-orange-600 text-slate-100 flex items-center justify-center mt-3 md:mt-10">
-      TODO
+    <div className="w-full h-24 bg-orange-600 flex flex-col items-center justify-end mt-3 md:mt-10">
+      <div className="flex items-center space-x-6 text-slate-100">
+        <div className="flex space-x-4">
+          <a href="#">
+            <FaInstagram className="text-xl md:text-2xl lg:text-3xl transition duration-300 transform hover:scale-110" />
+          </a>
+          <a href="#">
+            <FaFacebook className="text-xl md:text-2xl lg:text-3xl transition duration-300 transform hover:scale-110" />
+          </a>
+          <a href="#">
+            <FaYoutube className="text-xl md:text-2xl lg:text-3xl transition duration-300 transform hover:scale-110" />
+          </a>
+        </div>
+      </div>
+      <div className="text-slate-100 italic font-serif text-sm mt-2">
+        Designed by{" "}
+        <a
+          className="text-slate-100 italic font-serif text-sm mt-2 hover:underline"
+          href="https://muratcan23.github.io/myportfolio/"
+          target="_blank"
+        >
+          MC
+        </a>
+      </div>
+      <div className="text-slate-100 mt-1">
+        All rights reserved {new Date().getFullYear()}
+      </div>
     </div>
   );
 };
